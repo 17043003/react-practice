a8b7933712a2152dae017ffaa167cd7f3ea5931f
17043003 <ishizuka533725@gmail.com>
2021-06-14 01:33:56 +0900

    add reducer to increment and decrement

--- src/src/actions/index.js
+++ src/src/actions/index.js
@@ -1,6 +1,7 @@
 export const INCREMENT = 'INCREMENT'
 export const DECREMENT = 'DECREMENT'
 
+// action creator
 export const increment = () => ({
   type: INCREMENT
 })
---
+++ src/src/reducers/index.js
@@ -0,0 +1,4 @@
+import { combineReducers } from 'redux' // reducersをまとめる
+import count from './count'
+
+export default combineReducers({ count })
