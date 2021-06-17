28f793dcb7f7768f890e2b238f487da428de3adf
17043003 <ishizuka533725@gmail.com>
2021-06-17 22:33:13 +0900

    delete event

--- src/src/index.js
+++ src/src/index.js
@@ -2,18 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-
+import { composeWithDevTools } from 'redux-devtools-extension'
 
 import './index.css';
 import EventsIndex from './components/events_index';
 import EventsNew from './components/events_new';
+import EventsShow from './components/events_show'
+
 import reportWebVitals from './reportWebVitals';
 import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
 
 import reducer from './reducers'
 
-const store = createStore(reducer, applyMiddleware(thunk))
+const enhancer = process.env.NODE_ENV === 'development' ?
+  composeWithDevTools(applyMiddleware(thunk)) : applyMiddleware(thunk)
+const store = createStore(reducer, enhancer)
 
 
 ReactDOM.render(
@@ -21,8 +25,10 @@ ReactDOM.render(
     <Provider store={store}>
       <BrowserRouter>
         <Switch>
-          <Route exact path="/events/new" component={EventsNew} />
+          <Route path="/events/new" component={EventsNew} />
+          <Route path="/events/:id" component={EventsShow} />
           <Route exact path="/" component={EventsIndex} />
+          <Route exact path="/events" component={EventsIndex} />
         </Switch>
       </BrowserRouter>
     </Provider>
